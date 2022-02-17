974199679e2e440dd7c262f0d3679db3844e7438
Ethan Nourbash <ethannourbash2022@u.northwestern.edu>
2022-02-17 16:42:31 -0600

    Change raw data to zip level

--- zillow_cleaning.R
+++ zillow_cleaning.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8875cfe10ed7447dcac6011349755307f96409b4710f05c217ec45f1910e98fe
-size 583
+oid sha256:33b4a112b2823b4018e700e04571528ba093eef12bf58db4b79f382f1d9cfa3c
+size 574
