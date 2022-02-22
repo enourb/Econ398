31a579d6b18fa982d90fc5547eee4a60fffea631
Ethan Nourbash <ethannourbash2022@u.northwestern.edu>
2022-02-21 20:50:37 -0600

    Actually add age variable

--- nhgis_cleaning.R
+++ nhgis_cleaning.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ceccd38b5fae77b45483664450a7ca12cc57d85a7d81e7d5b0a1ad37b37210
-size 5544
+oid sha256:6055e4de5e391399bd0c9dc86f553e77816a3dc741868bb5f3e5c398c10d6ffb
+size 5548
