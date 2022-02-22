1d98798d2cfba2f8cfb208977d1721d0b6244bab
Ethan Nourbash <ethannourbash2022@u.northwestern.edu>
2022-02-21 20:35:23 -0600

    EDA stuff

--- eda.R
+++ eda.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f307f2f725ca79b9e4ea49a5c86b27b02e7aaadaddec26fee36b7145891e5438
-size 4550
+oid sha256:219906aeec7b91ce37d55231e02cbea936756855c0ebff41232a0e141d431052
+size 4594
